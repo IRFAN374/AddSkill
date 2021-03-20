4ec8a85185155f91e24c36be9bbc635ed88e836b
IRFAN374 <irfanmd2503@gmail.com>
2021-03-20 17:14:42 +0530

    Textgear key is enter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// import textgears from 'textgears-api';
+
+// const textgearsApi = textgears('QzDHhSFOTJUTOswK', {language: 'en-US'});
+// textgearsApi.checkGrammar('I is a engineer')
+//     .then((data) => {
+//         for (const error of data.errors) {
+//             console.log('Error: %s. Suggestions: %s', error.bad, error.better.join(', '));
+//         }
+//     })
+//     .catch((err) => {});
+
+
+
 function loadFileAsText(){
     var fileToLoad = document.getElementById("fileToLoad").files[0];
   
